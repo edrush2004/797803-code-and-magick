2d9fd5f7cd4b8754f9a2404e8fa2c9821fcf9c41
DmitryAgeev <ageevdmitry2018@gmail.com>
2018-12-26 22:29:27 +0300

    Первоначальная версия задания

--- js/setup.js
+++ js/setup.js
@@ -68,7 +68,7 @@ var onPopupEscPress = function (evt) {
   }
 
   if (document.activeElement.closest('.setup-user-name')) {
-    evt.stopPropagation();
+    document.querySelector('.setup-user-name').blur();
   } else {
     closePopup();
   }
@@ -104,3 +104,55 @@ setupClose.addEventListener('keydown', function (evt) {
   }
 });
 
+(function () {
+
+  var dialogHandler = setup.querySelector('.upload');
+
+  dialogHandler.addEventListener('mousedown', function (evt) {
+    evt.preventDefault();
+
+    var startCoords = {
+      x: evt.clientX,
+      y: evt.clientY
+    };
+
+    var dragged = false;
+
+    var onMouseMove = function (moveEvt) {
+      moveEvt.preventDefault();
+      dragged = true;
+
+      var shift = {
+        x: startCoords.x - moveEvt.clientX,
+        y: startCoords.y - moveEvt.clientY
+      };
+
+      startCoords = {
+        x: evt.clientX,
+        y: evt.clientY
+      };
+
+      setup.style.top = (setup.offsetTop - shift.y) + 'px';
+      setup.style.left = (setup.offsetTop - shift.x) + 'px';
+    };
+
+    var onMouseUp = function (upEvt) {
+      upEvt.preventDefault();
+
+      document.removeEventListener('mousemove', onMouseMove);
+      document.removeEventListener('mouseup', onMouseUp);
+
+      if (dragged) {
+        var onClickPreventDefault = function (evt) {
+          evt.preventDefault();
+          dialogHandler.removeEventListener('click', onClickPreventDefault);
+        };
+        dialogHandler.addEventListener('click', onClickPreventDefault);
+      }
+    };
+
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', onMouseUp);
+  });
+
+})();
