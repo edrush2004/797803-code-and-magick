b8289812bc5f4947da0008c063e0383aa38433d7
Keks <keks@htmlacademy.ru>
2019-01-08 22:32:36 +0300

    :heavy_check_mark: Сборка #7

---
+++ 7/js/dialog.js
@@ -0,0 +1,53 @@
+'use strict';
+
+(function () {
+
+  var START_SETUP_POSITION_TOP = '80px';
+  var START_SETUP_POSITION_LEFT = '50%';
+  var setup = document.querySelector('.setup');
+  var setupOpen = document.querySelector('.setup-open');
+  var setupClose = setup.querySelector('.setup-close');
+  var setupUserName = document.querySelector('.setup-user-name');
+  var form = setup.querySelector('.setup-wizard-form');
+
+  var onPopupEscPress = function (evt) {
+    if (setupUserName !== document.activeElement) {
+      window.util.isEscEvent(evt, closePopup);
+    }
+  };
+
+  setupOpen.addEventListener('click', function () {
+    openPopup();
+  });
+
+  setupOpen.addEventListener('keydown', function (evt) {
+    window.util.isEnterEvent(evt, openPopup);
+  });
+
+  setupClose.addEventListener('click', function () {
+    closePopup();
+  });
+
+  setupClose.addEventListener('keydown', function (evt) {
+    window.util.isEnterEvent(evt, closePopup);
+  });
+
+  var openPopup = function () {
+    setup.classList.remove('hidden');
+    document.addEventListener('keydown', onPopupEscPress);
+  };
+
+  var closePopup = function () {
+    setup.classList.add('hidden');
+    document.removeEventListener('keydown', onPopupEscPress);
+    setup.style.top = START_SETUP_POSITION_TOP;
+    setup.style.left = START_SETUP_POSITION_LEFT;
+  };
+
+  form.addEventListener('submit', function (evt) {
+    evt.preventDefault();
+    window.backend.save(new FormData(form), function () {
+      setup.classList.add('hidden');
+    });
+  });
+})();
---
+++ 7/js/util.js
@@ -0,0 +1,20 @@
+'use strict';
+
+(function () {
+  var ECS_KEYCODE = 27;
+  var ENTER_KEYCODE = 13;
+
+  window.util = {
+    isEscEvent: function (evt, action) {
+      if (evt.keyCode === ECS_KEYCODE) {
+        action();
+      }
+    },
+
+    isEnterEvent: function (evt, action) {
+      if (evt.keyCode === ENTER_KEYCODE) {
+        action();
+      }
+    }
+  };
+})();
