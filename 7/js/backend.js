395af214feca403cba02fe2f27bc658dd306d119
Keks <keks@htmlacademy.ru>
2019-01-10 00:15:57 +0300

    :heavy_check_mark: Сборка #7

--- 7/js/backend.js
+++ 7/js/backend.js
@@ -2,10 +2,9 @@
 
 (function () {
 
-
   window.backend = {
 
-    save: function (data, onLoad) {
+    save: function (data, onLoad, onError) {
 
       var URL = 'https://js.dump.academy/code-and-magick';
 
@@ -13,8 +12,21 @@
       xhr.responseType = 'json';
 
       xhr.addEventListener('load', function () {
-        onLoad(xhr.response);
+        if (xhr.status === 200) {
+          onLoad(xhr.response);
+        } else {
+          onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
+        }
       });
+
+      xhr.addEventListener('error', function () {
+        onError('Произошла ошибка соединения');
+      });
+      xhr.addEventListener('timeout', function () {
+        onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
+      });
+
+      xhr.timeout = 10000;
 
       xhr.open('POST', URL);
       xhr.send(data);
@@ -46,8 +58,8 @@
 
       xhr.open('GET', URL);
       xhr.send();
-
     }
   };
+
 })();
 
--- 7/js/otherMagicks.js
+++ 7/js/otherMagicks.js
@@ -15,7 +15,7 @@
     return wizardElement;
   };
 
-  var successHandler = function (wizards) {
+  var otherMagicks = function (wizards) {
     var fragment = document.createDocumentFragment();
 
     for (var i = 0; i < 4; i++) {
@@ -26,18 +26,6 @@
     document.querySelector('.setup-similar').classList.remove('hidden');
   };
 
-  var errorHandler = function (errorMessage) {
-    var node = document.createElement('div');
-    node.style = 'z-index: 100; margin: 0 auto; text-align: center; background-color: red;';
-    node.style.position = 'absolute';
-    node.style.left = 0;
-    node.style.right = 0;
-    node.style.fontSize = '30px';
-
-    node.textContent = errorMessage;
-    document.body.insertAdjacentElement('afterbegin', node);
-  };
-
-  window.backend.load(successHandler, errorHandler);
+  window.backend.load(otherMagicks);
 
 })();
