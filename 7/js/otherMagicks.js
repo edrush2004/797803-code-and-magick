0d03c04a51653f23123e88daf1399f4c70f30ee8
Keks <keks@htmlacademy.ru>
2019-01-10 22:13:45 +0300

    :heavy_check_mark: Сборка #7

--- 7/js/otherMagicks.js
+++ 7/js/otherMagicks.js
@@ -2,6 +2,8 @@
 
 (function () {
 
+  var quantityPopapWizards = 4;
+
   var similarListElement = document.querySelector('.setup-similar-list');
 
   var similarWizardTemplate = document.querySelector('#similar-wizard-template').content;
@@ -18,14 +20,18 @@
   var otherMagicks = function (wizards) {
     var fragment = document.createDocumentFragment();
 
-    for (var i = 0; i < 4; i++) {
-      fragment.appendChild(renderWizard(wizards[i]));
+    var getRandomIndex = function (min, max) {
+      return Math.floor(min + Math.random() * (max + 1 - min));
+    };
+
+    for (var i = 0; i < quantityPopapWizards; i++) {
+      fragment.appendChild(renderWizard(wizards[getRandomIndex(0, wizards.length - 1)]));
     }
     similarListElement.appendChild(fragment);
 
     document.querySelector('.setup-similar').classList.remove('hidden');
   };
 
-  window.backend.load(otherMagicks);
+  window.backend.load(otherMagicks, window.errorMessage.rendErrorMessage);
 
 })();
